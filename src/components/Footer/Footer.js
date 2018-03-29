e867b01eb09d1b1968cd3cf02e1419e4f59a22f5
calaca <lorena.calaca.od@gmail.com>
2018-03-29 16:22:07 -0300

    styles: add footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import ReactIcon from '../../assets/imgs/logo.svg';
 
 class Footer extends Component {
   render() {
-    return <footer>Footer</footer>
+    return (
+      <footer>
+        <p className="made-with">
+          made with
+          <img src={ReactIcon} alt="React" title="React"/>
+          by <a href="https://calaca.github.io/">calaca</a>
+        </p>
+      </footer>
+    )
   }
 }
 
