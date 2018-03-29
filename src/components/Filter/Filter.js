120ab0f7d812280a704fede6915fe501b5ecb1c3
calaca <lorena.calaca.od@gmail.com>
2018-03-29 15:47:49 -0300

    styles: add filter styles

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 import './Filter.css';
+import FilterIcon from '../../assets/imgs/funnel.svg';
 
 class Filter extends Component {
   render() {
-    return <aside className="filter">Filter</aside>
+    return (
+      <aside className="filter">
+        <h2 className="filter-title">Filter Search</h2>
+        <div className="input-wrapper">
+          <input type="text" placeholder="Type a location name here" aria-label="Type a location name here"/>
+          <button className="filter-btn" aria-label="Filter">
+            <img src={FilterIcon} alt="Filter" className="icon" title="Filter"/>
+          </button>
+        </div>
+        <div className="results">
+          <p className="results-summary">
+            Showing <strong>4</strong> of <strong>4</strong> places.
+          </p>
+          <ul className="results-list">
+            <li className="result-item">
+              Bistrô Maria Doceu
+            </li>
+            <li className="result-item">
+              Casebre Restaurante
+            </li>
+            <li className="result-item">
+              Montserrat Gastronomia
+            </li>
+            <li className="result-item">
+              Santa Dica Cervejaria Artesanal
+            </li>
+          </ul>
+        </div>
+      </aside>
+    )
   }
-}
+};
 
 export default Filter;
