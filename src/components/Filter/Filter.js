fb1e0d69419cc7ffeafaab75af33357d6e93e8d4
calaca <lorena.calaca.od@gmail.com>
2018-04-10 15:39:35 -0300

    feat: link markers to result list

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Debounce } from 'react-throttle';
 import './Filter.css';
 import FilterIcon from '../../assets/imgs/funnel.svg';
 
@@ -8,14 +9,18 @@ class Filter extends Component {
     showingPlaces: []
   }
 
-  // TODO: link infowindows with result list
-  // TODO: get extra data from third party API
-
+  /**
+  * @description Sets initial state for result list, markers, etc.
+  */
   componentDidMount() {
     // set initial result list, markers and friends
     this.filterPlaces(this.state.query);
   }
 
+  /**
+  * @description Filter places to show
+  * @param {string} query - The search query
+  */
   filterPlaces = (query) => {
     const { places, infowindow } = this.props.data;
     let showingPlaces;
@@ -28,6 +33,13 @@ class Filter extends Component {
     // close all infowindows when the user starts typing
     infowindow.close();
 
+    // hide all markers
+    places.forEach(place => {
+      if (place.marker) {
+        place['marker'].setMap(null);
+      }
+    });
+
     // filter the places to show
     if (query) {
       showingPlaces = places.filter(place => place['name'].toLowerCase().includes(query));
@@ -38,12 +50,49 @@ class Filter extends Component {
     // set places to show in state
     this.setState({ showingPlaces });
 
-    // create and show markers for each place
+    // create and show markers for each place that matches the query
     this.createMarkers(showingPlaces);
   }
 
+  /**
+  * @description Create infowindow content
+  * @param {object} place - The place object containing information about it
+  */
+  createContent = (place) => {
+    return `<p>This is the content for ${place.name}</p>`;
+  }
+
+  /**
+  * @description Animate marker to bounce once
+  * @param {object} marker - The place marker
+  */
+  markerAnimation = (marker) => {
+    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    setTimeout(() => {
+      marker.setAnimation(null);
+    }, 375);
+  }
+
+  /**
+  * @description Configure and set the infowindow
+  * @param {object} map - The map
+  * @param {object} place - The infowindow place
+  * @param {object} infowindow - The infowindow itself
+  * @param {object} marker - The place marker
+  */
+  setInfowindow = (map, place, infowindow, marker) => {
+    infowindow.setContent(this.createContent(place));
+    infowindow.open(map, marker);
+    this.markerAnimation(marker);
+  }
+
+/**
+  * @description Create a marker for each place and save it in the place object
+  * @param {array} places - The places
+  */
   createMarkers = (places) => {
     const { map, infowindow, bounds } = this.props.data;
+    const self = this;
 
     places.forEach(place => {
       // create a marker for each place
@@ -53,17 +102,10 @@ class Filter extends Component {
         title: place.name,
         animation: window.google.maps.Animation.DROP
       });
-
-      // create a listener for a click and set the infowindow for each marker
+      
+      // create a listener for a click and link it to the infowindow
       place.marker.addListener('click', function() {
-        infowindow.setContent(`<p>This is the content for ${place.name}</p>`);
-        infowindow.open(map, this);
-
-        // make marker bounce only once
-        this.setAnimation(window.google.maps.Animation.BOUNCE);
-        setTimeout(() => {
-          this.setAnimation(null);
-        }, 375);
+        self.setInfowindow(map, place, infowindow, this);
       });
 
       // extends map bounds to the marker
@@ -74,6 +116,16 @@ class Filter extends Component {
     map.fitBounds(bounds);
   }
 
+  /**
+  * @description Open a infowindow when the respective list item is clicked
+  * @param {event} e - The event
+  * @param {object} place - The infowindow place
+  */
+  openInfowindow = (e, place) => {
+    const { map, infowindow } = this.props.data;
+    this.setInfowindow(map, place, infowindow, place.marker);
+  }
+
   render() {
     const { places } = this.props.data;
     const { showingPlaces } = this.state;
@@ -82,12 +134,14 @@ class Filter extends Component {
       <aside className="filter">
         <h2 className="filter-title">Filter Search</h2>
         <div className="input-wrapper">
-          <input 
-            type="text"
-            placeholder="Type a location name here"
-            aria-label="Type a location name here"
-            onChange={e => this.filterPlaces(e.target.value)}
-          />
+          <Debounce time="300" handler="onChange">
+            <input 
+              type="text"
+              placeholder="Type a location name here"
+              aria-label="Type a location name here"
+              onChange={e => this.filterPlaces(e.target.value)}
+            />
+          </Debounce>
           <button className="filter-btn" aria-label="Filter">
             <img src={FilterIcon} alt="Filter" className="icon" title="Filter"/>
           </button>
@@ -103,9 +157,13 @@ class Filter extends Component {
           }
           <ul className="results-list">
             {
-              showingPlaces.map(p => (
-                <li key={p.id} className="result-item">
-                  {p.name}
+              showingPlaces.map(place => (
+                <li
+                  key={place.id}
+                  className="result-item"
+                  onClick={e => this.openInfowindow(e, place)}
+                >
+                  {place.name}
                 </li>
               ))
             }
